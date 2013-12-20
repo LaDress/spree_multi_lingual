111ec8d90f72f07fa94b2590d658236e5940b8c2
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-12-20 12:33:54 +0100

    Remove unwanted translation tables

--- app/overrides/property_add_language_dropdown.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  :virtual_path => "spree/admin/properties/edit",
-  :insert_before=> "erb[loud]:contains(\"render :partial => 'form'\")",
-  :text         => '<%= render "spree/admin/shared/language_dropdown", :object => @property -%>',
-  :name         => "property_add_language_dropdown"
-)
-
