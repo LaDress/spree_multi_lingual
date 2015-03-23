ea5eabd548df4546e11cdc82a15fe1012c8a47d0
Matijs van Zuijlen <matijs@matijs.net>
2015-03-23 18:35:30 +0100

    Make sure spree_multi_lingual doesn't mess things up

--- lib/spree_multi_lingual/controller_helper_locale.rb
+++ lib/spree_multi_lingual/controller_helper_locale.rb
@@ -6,19 +6,14 @@ module SpreeMultiLingual::ControllerHelperLocale
     base.class_eval do
       private
 
-      # Internal : Set user I18n.locale
-      #
-      # override spree_core set_user_language
-      # initialize user language :
-      #  params[:locale] is explicitly given by routing-filter /:locale
-      # or the session[:locale] set by Spree::LocaleController#set
-      # or I18n.locale set by rack-locale for example
-      # or Spree::Config[:default_locale]
-      # or Rails.application.config.i18n.default_locale
+      # HACK: Made to work like we want it.
       def set_user_language
-        locale = params[:locale] || session[:locale] || I18n.locale || Rails.application.config.i18n.default_locale
-        locale = I18n.default_locale unless locale && I18n.available_locales.include?(locale.to_sym)
-        I18n.locale = locale.to_sym
+        locale = params[:locale]
+        unless locale && I18n.available_locales.include?(locale.to_sym)
+          locale = I18n.default_locale
+        end
+        I18n.locale = locale
+        session[:locale] = I18n.locale
       end
     end
   end
